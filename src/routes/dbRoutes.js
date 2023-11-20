7b1ace59ee674f761362ed777218d72b50cfcb1c
Ataxia123 <vp.ca94@gmail.com>
2023-11-20 16:23:09 -0500

    think

--- src/routes/dbRoutes.js
+++ src/routes/dbRoutes.js
@@ -163,11 +163,11 @@ dbRouter.post('/attest', async (req, res) => {
   async function payRespects(playerData) {
     // Save only if player id does not exist
     const attestationData = {
-      id: playerData._id,
+      id: playerData.id,
       Attestation: JSON.parse(playerData.attestation),
     };
     await respectsCol.updateOne(
-      { id: playerData._id },
+      { id: playerData.id },
       { $setOnInsert: attestationData },
       { upsert: true }, // this creates new document if none match the filter
     );
